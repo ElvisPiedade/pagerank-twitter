569394d907e8629130efedb320e29513da0106c0
Elvis Piedade <elvispiedade@yahoo.com.br>
2019-07-03 12:19:31 -0300

    Funcionalidade de adição de busca por tweets

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,11 +1,43 @@
+import config
+import pandas as pd
 import tweepy
+import json
 
-CONSUMER_KEY = 'jQRWMxNP3M0X9YJzlyx5w8eRJ'
-CONSUMER_SECRET = 'BpOnE9fnAfpMxewiRQND0gQdexqxiW9Yz1EhAr1ciJhminVk4a'
-ACCESS_KEY = '2875725285-D0ZBWRuRN5uv5PzSd8ToYriFAwbpb4EUXA4LJf4'
-ACCESS_SECRET = 'snqtYsn3p5I69G9UrwtTfkCYfUeuCovtw3KQBhhalF6Nu'
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status("Python-Test")
+#Search Tweets with query = machine learning
+def search_tweets():
+	max_tweets = 2
+	query = "machine learning"
+	searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+	
+	users_info = {}
+	
+	for tweets in searched_tweets:
+		users_info.update({tweets.user.id_str : {	"UserName" : tweets.user.name,
+													"ScreenName" : tweets.user.screen_name,
+													"FollowersCount" : tweets.user.followers_count,
+													"FriendsCount" : tweets.user.friends_count,
+													"Followers" : [follower for follower in tweepy.Cursor(api.followers_ids, screen_name=tweets.user.screen_name, wait_on_rate_limit=True).items(10)],
+													"Friends" : [friend for friend in tweepy.Cursor(api.friends_ids, screen_name=tweets.user.screen_name, wait_on_rate_limit=True).items(5)]
+												}
+							})
+
+	users_df = pd.DataFrame(users_info)
+
+	return users_df
+
+def read_file():
+	df = pd.read_csv('my_csv.csv', index_col= 0)
+	return df
+
+
+df2 = read_file()
+usersdf = search_tweets()
+for x in usersdf:
+	df2[x] = usersdf[x]
+
+df2.to_csv('my_csv.csv')
+
